f9e6626298d19e4db013ef9a173f1784db53250e
dont-att-me <74231174+dont-att-me@users.noreply.github.com>
2021-09-20 02:21:53 -0400

    Re-implemented DrawCSSText, Included DRAW_SPRITE wrapper
    Added Alignment, and Font Enum's
    Added a few UI (X, Y W, H) Constants.
    Implemented DRAW_SPRITE`wrapper function DrawSprite(...)

--- src/UILibrary/script.h
+++ src/UILibrary/script.h
@@ -12,6 +12,61 @@
 
 #include "..\..\inc\main.h"
 
+enum class Font : int {
+	Arial,
+	Cabrito,
+	DroidSerif,
+	Hapna,
+	Helvetica,
+	Catalogue,
+	Chalk,
+	Gothica,
+	Ledger,
+	Lino,
+	Turri,
+	Redemption,
+	Gamertag,
+	DreamerScript,
+};
+enum class Alignment : int {
+	Left,
+	Center,
+	Right
+};
+
+const float SCREEN_WIDTH = 1920.0f;
+const float SCREEN_HEIGHT = 1080.0f;
+
+const float BG_X_OFFSET = 25.0f;
+const float BG_Y_OFFSET = 25.0f;
+const float BG_WIDTH    = 576.0f; 
+const float BG_HEIGHT   = 1026.0f;
+
+const float TOP_HEADER_WIDTH  = 442.0f; 
+const float TOP_HEADER_HEIGHT = 108.0f;
+const float TOP_HEADER_X_POS = BG_X_OFFSET + ((BG_WIDTH * 0.5f) - (TOP_HEADER_WIDTH * 0.5f));
+const float TOP_HEADER_Y_POS = BG_Y_OFFSET + ((BG_WIDTH - TOP_HEADER_WIDTH) * 0.5f);
+
+const float FOOTER_LINE_WIDTH  = 422.0f; 
+const float FOOTER_LINE_HEIGHT = 1.0f;
+const float FOOTER_LINE_X_POS = BG_X_OFFSET + ((BG_WIDTH * 0.5f) - (FOOTER_LINE_WIDTH * 0.5f));
+const float FOOTER_LINE_Y_POS = BG_Y_OFFSET + (BG_HEIGHT - ((BG_WIDTH - FOOTER_LINE_WIDTH) * 0.5f));
+
+// Y Increment for sprite and text position
+const float INCREMENT = (SCREEN_HEIGHT * 0.053f); // const float INCREMENT = 57;
+
+inline void DrawSprite(const char* textureDict, const char* textureName, float screenX, float screenY, float width, float height, float heading, int red, int green, int blue, int alpha, BOOL Centered) {
+		screenX /= SCREEN_WIDTH;
+		screenY /= SCREEN_HEIGHT;
+		width   /= SCREEN_WIDTH;
+		height  /= SCREEN_HEIGHT;
+		if (!Centered) {
+			screenX += width * 0.5f;
+			screenY += height * 0.5f;
+      }
+		GRAPHICS::DRAW_SPRITE(textureDict, textureName, screenX, screenY, width, height, heading, red, green, blue, alpha, false);
+	}
+
 void ScriptMain();
 
 int GetCurrentSelectedIndex();
