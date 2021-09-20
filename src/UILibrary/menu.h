c403307c00fed46598d8d96f6928f27f83bea9dc
dont-att-me <74231174+dont-att-me@users.noreply.github.com>
2021-09-20 02:21:48 -0400

    Re-implemented DrawCSSText

--- src/UILibrary/menu.h
+++ src/UILibrary/menu.h
@@ -6,7 +6,7 @@
 #include "script.h"
 
 // Draws CSS styled text
-void DrawCSSText(std::string text, std::string font, std::string color, std::string align, int leading, int rightMargin, int textSize, float X, float Y);
+void DrawCSSText(std::string text, Font font, int R, int G, int B, int A, Alignment align, int textSize, float X, float Y, int WrapWidth = 0, int LetterSpacing = 0);
 void DrawListOption(std::string text, int index);
 // On & Off Toggle Option
 void DrawToggleOption(std::string text, int index);
