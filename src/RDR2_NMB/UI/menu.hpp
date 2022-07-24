774a8d28808edbe9ed0d088c862fd3e71dfd4ab3
Halen84 <55899402+Halen84@users.noreply.github.com>
2022-07-24 16:03:07 -0500

    Move script.h constants to menu.hpp

--- src/RDR2_NMB/UI/menu.hpp
+++ src/RDR2_NMB/UI/menu.hpp
@@ -14,6 +14,28 @@
 #endif
 
 
+const float SCREEN_WIDTH = 1920.0f;		// TODO: Maybe convert to use Util::g_screenWidth?
+const float SCREEN_HEIGHT = 1080.0f;	// TODO: Maybe convert to use Util::g_screenHeight?
+
+const float BG_X_OFFSET = 25.0f;		// Background X Offset
+const float BG_Y_OFFSET = 25.0f;		// Background Y Offset
+const float BG_WIDTH    = 576.0f;		// Background Width
+const float BG_HEIGHT   = 1026.0f;		// Background Height
+
+const float TOP_HEADER_WIDTH  = 442.0f;	// Header Width
+const float TOP_HEADER_HEIGHT = 108.0f;	// Header Height
+const float TOP_HEADER_X_POS = BG_X_OFFSET + ((BG_WIDTH * 0.5f) - (TOP_HEADER_WIDTH * 0.5f));	// Header X Offset
+const float TOP_HEADER_Y_POS = BG_Y_OFFSET + ((BG_WIDTH - TOP_HEADER_WIDTH) * 0.2f);			// Header Y Offset
+
+//const float FOOTER_LINE_WIDTH  = 422.0f; 
+//const float FOOTER_LINE_HEIGHT = 2.0f;
+//const float FOOTER_LINE_X_POS = BG_X_OFFSET + ((BG_WIDTH * 0.5f) - (FOOTER_LINE_WIDTH * 0.5f));
+//const float FOOTER_LINE_Y_POS = BG_Y_OFFSET + (BG_HEIGHT - ((BG_WIDTH - FOOTER_LINE_WIDTH) * 0.5f));
+
+// Y increment for option and sprite positions
+const float INCREMENT = (SCREEN_HEIGHT * 0.051f); // 55
+
+
 namespace Menu
 {
 	inline std::unordered_map<int, Submenu> g_SubmenusMap;
--- src/RDR2_NMB/script.h
+++ src/RDR2_NMB/script.h
@@ -8,25 +8,4 @@
 #include "..\..\inc\main.h"
 #include "common.hpp"
 
-const float SCREEN_WIDTH = 1920.0f;
-const float SCREEN_HEIGHT = 1080.0f;
-
-const float BG_X_OFFSET = 25.0f;
-const float BG_Y_OFFSET = 25.0f;
-const float BG_WIDTH    = 576.0f; 
-const float BG_HEIGHT   = 1026.0f;
-
-const float TOP_HEADER_WIDTH  = 442.0f; 
-const float TOP_HEADER_HEIGHT = 108.0f;
-const float TOP_HEADER_X_POS = BG_X_OFFSET + ((BG_WIDTH * 0.5f) - (TOP_HEADER_WIDTH * 0.5f));
-const float TOP_HEADER_Y_POS = BG_Y_OFFSET + ((BG_WIDTH - TOP_HEADER_WIDTH) * 0.2f);
-
-const float FOOTER_LINE_WIDTH  = 422.0f; 
-const float FOOTER_LINE_HEIGHT = 2.0f;
-const float FOOTER_LINE_X_POS = BG_X_OFFSET + ((BG_WIDTH * 0.5f) - (FOOTER_LINE_WIDTH * 0.5f));
-const float FOOTER_LINE_Y_POS = BG_Y_OFFSET + (BG_HEIGHT - ((BG_WIDTH - FOOTER_LINE_WIDTH) * 0.5f));
-
-// Y Increment for sprite and text position
-const float INCREMENT = (SCREEN_HEIGHT * 0.051f); // 55;
-
 void ScriptMain();
